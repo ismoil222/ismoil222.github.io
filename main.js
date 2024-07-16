da3c1c291420bf44f9a97fa18537d3fca7f891b8
Ismoilov <whatcool999@gmail.com>
2024-07-16 18:35:33 +0500

    Elnura

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function AutoClick() {
     }
 }
 
-    clickerlvltext.innerText = "Clicker- Lvl " + clickerlvl + " - Cost: " + clicklvlcost;
+    clickerlvltext.innerText = "Multitap Lvl " + clickerlvl + " - Cost: " + clicklvlcost;
 
     autolvltext.innerText = "Buy Auto Click -" + " Cost: " + autocost;
 function point() {
@@ -44,22 +44,3 @@ function point() {
     var scoreshow = document.getElementById('scoreshow')
     scoreshow.innerText = "" + score;
 }
-
-$(document).ready(function(){
-    var stars=800;
-    var $stars=$(".stars");
-    var r=800;
-    for(var i=0;i<stars;i++){
-        var $star=$("<div/>").addClass("star");
-        $stars.append($star);
-    }
-    $(".star").each(function(){
-            var cur=$(this);
-            var s=0.2+(Math.random()*1);
-            var curR=r+(Math.random()*300);
-            cur.css({ 
-            transformOrigin:"0 0 "+curR+"px",
-            transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")" 
-        })
-    })
-})
